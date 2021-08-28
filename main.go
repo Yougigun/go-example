004e7b48ccb04015ed0e8b05a9d3e27d52889d0e
gary.y <gary.y@binance.com>
2021-08-28 15:56:19 +0800

    test panic

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-
-	c := make(<-chan int,1)
-	var c1 chan<- int
-	c1 = c
-	fmt.Println(c1)
+	// gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	// router := gin.New()
+	// router.Use(gin.Recovery())
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	router.GET("/panic", func(c *gin.Context) {
+		panic("testing panic")
+	})
+	router.GET("/ok", func(c *gin.Context) {
+		c.JSON(200,"ok")
+	})
+	router.Run(":8080")
 }
