de18a10e6027b5f5fc61e3170152809850f6f5b4
gary.y <gary.y@binance.com>
2021-10-19 23:04:41 +0800

    json

--- json/main.go
+++ json/main.go
@@ -5,9 +5,30 @@ import (
 	"fmt"
 )
 
+type People struct{
+	Name string `json:"name"`
+}
+
+type Doctor struct{
+	People
+	Career string `json:"career"`
+	Name string `json:"name"`
+}
+
 func main() {
 	m := map[string]interface{}{}
 	// err:=json.Unmarshal([]byte("{\"aa\":\"xxx\"}"),&m)
 	err:=json.Unmarshal([]byte(""),&m)
 	fmt.Println(err,m)
+
+	d := Doctor{
+		People: People{Name: "gary"},
+		Career: "doctor",
+	}
+	bs,err := json.Marshal(d)
+	fmt.Println(string(bs))
+	fmt.Println("err: ",err)
+	d2 := Doctor{}
+	err = json.Unmarshal(bs,&d2)
+	fmt.Printf("%+v, err: %v\n",d2,err)
 }
